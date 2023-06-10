88f9ba4dccc19015e3e95a61a7ddd6a8e0751cfe
Gabriel Pessoa <tentandodrop@gmail.com>
2023-06-10 16:46:47 -0300

    Adding initial structure

---
+++ src/main/java/br/com/gabriepessoa/orderorchestratorapi/OrderOrchestratorApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabriepessoa.orderorchestratorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderOrchestratorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderOrchestratorApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/gabriepessoa/orderorchestratorapi/OrderOrchestratorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabriepessoa.orderorchestratorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderOrchestratorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
